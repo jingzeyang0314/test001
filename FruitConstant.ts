13dd78c4caa0f65781b295ee250010029b8c154e
jingzeyang0314 <42266320+jingzeyang0314@users.noreply.github.com>
2019-06-10 17:43:59 +0800

    水果constant

---
+++ FruitConstant.ts
@@ -0,0 +1,20 @@
+
+export default class FruitConstant {
+    public static ItemType = cc.Enum ({
+        CAKE:0,
+        TOOLS:1,
+        CHIEF:2,
+    })
+
+    public static KniefType = cc.Enum({
+        NORMAL:0,
+        KNIEF1:1,
+        KNIEF2:2,
+    })
+
+
+    public static SoundConfig = cc.Enum({
+        READY_GO:0,
+    })
+
+}
